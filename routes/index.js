24118c3114d4846b93099a5045611b2947b51885
hjbarangan <hjbarangan@newbrighton.edu.ph>
2023-05-23 18:12:39 +0800

    fix: Modified method in deleting the table

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,6 @@ const makeExpressCallback = require("../express-callback");
 const { deleteTableController } = require("../controllers/delete-table");
 const { getTableController } = require("../controllers/get-table");
 
-router.post("/delete-chk-transaction", makeExpressCallback(deleteTableController));
+router.delete("/delete-chk-transaction", makeExpressCallback(deleteTableController));
 router.get("/get-chk-transaction", makeExpressCallback(getTableController));
 module.exports = router;
